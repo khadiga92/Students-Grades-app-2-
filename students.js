2d546262202def986bdf15cedbd353fe21c907fd
khadiga92 <khadiga92@hotmail.com>
2021-12-14 19:23:51 +0200

    Added sum to addGrade in student.js

--- students.js
+++ students.js
@@ -5,21 +5,24 @@ const addGrade = (id, name, grades, comment) => {
     const duplicateTitles = students.filter((student) => {
         return student.id === id;
     });
+
     if (duplicateTitles.length === 0) {
+        sum = 0 // [2,3,5]
+        grades.forEach((grade)=>{
+            sum+= grade  // sum = sum + grade 0 + 1 =1 //1st    1 + 2 = 3 // 2nd loop  3 +8 = 11 3rd loop
+        })
         students.push({
             id,
             name,
             grades,
             comment,
-            //total: students.forEach((student)=>{
-            //sum = 0
-            // student.grades.forEach((grade)=> sum+= grade)
-            //})
-        });
+            sum
+            })
+        ;
         saveStudents(students);
         console.log("Saved Successfully");
     } else {
-        console.log("Error duplicate title");
+        console.log("Error duplicate");
     }
 };
 
@@ -41,14 +44,24 @@ const saveStudents = (student) => {
     fs.writeFileSync("students.json", saveData);
 };
 //////////////////////////////////////////////////////////////////////////////////
-const total = () => {
-    const students = loadstudents();
-    students.forEach((student) => {
-        sum = 0
-        student.grades.forEach((grade) => sum += grade)
-        saveStudents(students);
-    })
-}
+
+// const total = () => {
+//     const students = loadstudents();
+//     students.forEach((student) => {
+//         sum = 0
+//         student.grades.forEach((grade) => sum += grade)  
+//         console.log(sum)
+//     })
+// }
+
+// const students = loadstudents();
+// const total = 
+//     students.forEach((student) => {
+//         sum = 0
+//         student.grades.forEach((grade) => sum += grade)  
+//         console.log(sum)
+//     })
+
 
 ////////////////////////////////////////////////////////////////////////////
 const deleteGrade = (id) => {
@@ -67,9 +80,9 @@ const readGrade = (id) => {
     const student = students.find((student) => {
         return student.id === id
     })
-    console.log(student)
+    //console.log(student)
     if (student) {
-        console.log(student.id, student.grades, total)
+        console.log(student.id, student.grades, student.sum)
     } else {
         console.log('Sorry not found')
     }
@@ -79,7 +92,7 @@ const readGrade = (id) => {
 const listGrades = () => {
     const students = loadstudents()
     students.forEach((student) => {
-        console.log(student.id, student.grades, total)
+        console.log(student.id, student.grades, student.sum)
     })
 }
 
